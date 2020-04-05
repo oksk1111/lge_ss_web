e6ef6621efb77aff08b861884b55bbee6b04f5d0
oksk1111 <oksk1111@gmail.com>
2020-04-05 22:00:36 +0900

    study until 2-7

---
+++ django_react/askcompany/instagram/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    message = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        #return f"Custom Post object ({self.id})"
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = "메시지 글자수"
